8e0b0c171e3b6c1d03cdc59925c6f3ef71c8278e
derkder <1026305391@qq.com>
2024-03-11 19:52:47 +0000

    fix: q2d代码增改

--- CW2/q2_d.m
+++ CW2/q2_d.m
@@ -1,70 +1,90 @@
-% 这里的意思好像是时间帧从1230 -》 1234，这四帧里它实现了闭环，然后整个结果都会好爆照多
-% 噢woc，就是我和小李看着那个圈圈邹一下缩小了那个地方
 % This script runs Q2(d)
 
-% Create the configuration object.
-configuration = drivebot.SimulatorConfiguration();
-
-% Enable the laser to support pure SLAM
-configuration.enableLaser = true;
-
-% For this part of the coursework, this should be set to false.
-configuration.perturbWithNoise = false;
-
+% Q2d
 % Set this value to truncate the run at a specified timestep rather than
 % run through the whole simulation to its end.
-configuration.maximumStepNumber = 1205;%通过改这里生成close前后的两组图
+maxStepNumbers = [1205, 1210];
+% Initialize arrays to store determinants for each iteration
+determinantsAll = zeros(length(maxStepNumbers), 7);
 
-% Set up the simulator
-simulator = drivebot.DriveBotSimulator(configuration, 'q2_d');
 
-% Create the localization system
-drivebotSLAMSystem = drivebot.DriveBotSLAMSystem(configuration);
-drivebotSLAMSystem.setRecommendOptimizationPeriod(inf);
 
-% Q2d:
-% Explore the  timestep where the loop closure occurs, and get
-% results just before and after the loop closure event
-warning('q2_d:unimplemented', ...
-        'Analyse loop closure behaviour for Q2d.')
+for stepIndex = 1:length(maxStepNumbers)
+    % Create the configuration object.
+    configuration = drivebot.SimulatorConfiguration();
+    % Enable the laser to support pure SLAM
+    configuration.enableLaser = true;    
+    % For this part of the coursework, this should be set to false.
+    configuration.perturbWithNoise = false;
+    configuration.maximumStepNumber = maxStepNumbers(stepIndex);
+    % Set up the simulator
+    simulator = drivebot.DriveBotSimulator(configuration, 'q2_d');
 
-% This tells the SLAM system to do a very detailed check that the input
-% appears to be correct but can make the code run slowly. Once you are
-% confident your code is working safely, you can set this to false.
-drivebotSLAMSystem.setValidateGraph(false);
+    % Create the localization system
+    drivebotSLAMSystem = drivebot.DriveBotSLAMSystem(configuration);
+    drivebotSLAMSystem.setRecommendOptimizationPeriod(inf);
+    
+    % This tells the SLAM system to do a very detailed check that the input
+    % appears to be correct but can make the code run slowly. Once you are
+    % confident your code is working safely, you can set this to false.
+    drivebotSLAMSystem.setValidateGraph(false);
+    
+    % Run the main loop and correct results
+    results = minislam.mainLoop(simulator, drivebotSLAMSystem);
+    
+    % Minimal output plots. For your answers, please provide titles and label
+    % the axes.
+    
+    % Plot optimisation times.
+    minislam.graphics.FigureManager.getFigure('Optimization times');
+    clf
+    plot(results{1}.optimizationTimes, '*')
+    hold on
+    
+    % Plot the error curves.
+    minislam.graphics.FigureManager.getFigure('Errors');
+    clf
+    plot(results{1}.vehicleStateHistory'-results{1}.vehicleStateHistory')
+    
+    % Plot covariance.
+    minislam.graphics.FigureManager.getFigure('Vehicle Covariances');
+    clf
+    plot(results{1}.vehicleCovarianceHistory')
+    hold on
+    
+    % Plot errors.
+    minislam.graphics.FigureManager.getFigure('Errors');
+    clf
+    plot(results{1}.vehicleStateHistory'-results{1}.vehicleTrueStateHistory')
+    hold on
+    
+    % Plot chi2 values.
+    minislam.graphics.FigureManager.getFigure('chi2 values');
+    clf
+    plot(results{1}.chi2Time, results{1}.chi2History)
+    hold on
+    
+    % Get landmark estimates after the simulation
+    [x, P, landmarkIds] = drivebotSLAMSystem.landmarkEstimates();
 
-% Run the main loop and correct results
-results = minislam.mainLoop(simulator, drivebotSLAMSystem);
+    % Calculate the determinants of the covariance matrices for each landmark
+    for i = 1:length(landmarkIds)
+        determinantsAll(stepIndex, i) = det(P(:,:,i));
+    end
 
-% Minimal output plots. For your answers, please provide titles and label
-% the axes.
+    % Print the differences in determinants for each landmark
+    for i = 1:length(landmarkIds)
+        fprintf('Landmark %d: Determinant = %f\n', landmarkIds(i), determinantsAll(stepIndex, i));
+    end
+    
+end
 
-% Plot optimisation times.
-minislam.graphics.FigureManager.getFigure('Optimization times');
-clf
-plot(results{1}.optimizationTimes, '*')
-hold on
+% Calculate the differences in determinants between the two iterations for each landmark
+determinantsDifferences = diff(determinantsAll, 1);
 
-% Plot the error curves.
-minislam.graphics.FigureManager.getFigure('Errors');
-clf
-plot(results{1}.vehicleStateHistory'-results{1}.vehicleStateHistory')
+% Print the differences in determinants for each landmark
+for i = 1:length(landmarkIds)
+    fprintf('Landmark %d: Determinant Difference = %f\n', landmarkIds(i), determinantsDifferences(i));
+end
 
-% Plot covariance.
-minislam.graphics.FigureManager.getFigure('Vehicle Covariances');
-clf
-plot(results{1}.vehicleCovarianceHistory')
-hold on
-
-% Plot errors.
-minislam.graphics.FigureManager.getFigure('Errors');
-clf
-plot(results{1}.vehicleStateHistory'-results{1}.vehicleTrueStateHistory')
-hold on
-
-% Plot chi2 values.
-minislam.graphics.FigureManager.getFigure('chi2 values');
-clf
-plot(results{1}.chi2Time, results{1}.chi2History)
-hold on
 
